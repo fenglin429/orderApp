f124b39a903eb216d9ea82fff42cad13169753c9
fenglin <248429457@qq.com>
2020-06-22 21:43:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vant from 'vant';
+import store from'./store/index'
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
